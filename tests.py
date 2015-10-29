35c9f880f39773d8ad49e8ff6def1774dde10f4f
Paweł Fertyk <pawel.fertyk@gmail.com>
2015-10-29 09:05:47 +0100

    Added more tests, also for pointy edge boards.

--- tests.py
+++ tests.py
@@ -6,23 +6,46 @@ class BoardGenerationTests(TestCase):
     def test_generate_board_rhombus(self):
         board = generate_board(5, 3, 'rhombus')
         expected_board = [
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
         ]
-        expected_board = [list(x) for x in zip(*expected_board)]
+        self.assertEqual(board, expected_board)
+
+    def test_generate_board_rhombus_pointy_top(self):
+        board = generate_board(5, 3, 'rhombus', pointy_top=True)
+        expected_board = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
         self.assertEqual(board, expected_board)
 
     def test_generate_board_rectangle(self):
         board = generate_board(5, 3, 'rectangular')
         expected_board = [
-            [0, 0, 0, 0, 1],
             [0, 0, 1, 1, 1],
-            [1, 1, 1, 1, 1],
-            [1, 1, 1, 1, 0],
-            [1, 1, 0, 0, 0],
+            [0, 0, 1, 1, 1],
+            [0, 1, 1, 1, 0],
+            [0, 1, 1, 1, 0],
+            [1, 1, 1, 0, 0],
         ]
-        expected_board = [list(x) for x in zip(*expected_board)]
+        self.assertEqual(board, expected_board)
+
+    def test_generate_board_rectangle_pointy_top(self):
+        board = generate_board(5, 3, 'rectangular', pointy_top=True)
+        expected_board = [
+            [0, 0, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 0],
+        ]
         self.assertEqual(board, expected_board)
 
     def test_generate_board_triangle(self):
@@ -34,17 +57,37 @@ class BoardGenerationTests(TestCase):
             [1, 1, 0, 0, 0],
             [1, 0, 0, 0, 0],
         ]
-        expected_board = [list(x) for x in zip(*expected_board)]
         self.assertEqual(board, expected_board)
 
-    def test_generate_board_hex(self):
-        board = generate_board(5, 5, 'hexagonal')
+    def test_generate_board_triangle_pointy_top(self):
+        board = generate_board(5, 3, 'triangular', pointy_top=True)
         expected_board = [
-            [0, 0, 1, 1, 1],
-            [0, 1, 1, 1, 1],
             [1, 1, 1, 1, 1],
             [1, 1, 1, 1, 0],
             [1, 1, 1, 0, 0],
+            [1, 1, 0, 0, 0],
+            [1, 0, 0, 0, 0],
         ]
-        expected_board = [list(x) for x in zip(*expected_board)]
+        self.assertEqual(board, expected_board)
+
+    def test_generate_board_hex(self):
+        board = generate_board(5, 3, 'hexagonal')
+        expected_board = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 0],
+        ]
+        self.assertEqual(board, expected_board)
+
+    def test_generate_board_hex_pointy_top(self):
+        board = generate_board(5, 3, 'hexagonal', pointy_top=True)
+        expected_board = [
+            [0, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 0],
+        ]
         self.assertEqual(board, expected_board)
